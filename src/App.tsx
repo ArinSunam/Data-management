2284664261ed6e826fbe3180d7404846e789a3ee
ArinSunam <arinsunam05@gmail.com>
2024-07-04 09:25:14 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-green-600">
+
+        <h1 className="text-red-500">hi</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
